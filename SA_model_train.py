beecca15a99eb17495b172e959aa11a35bc7a7af
Stanislav <56927835+Stanislavius@users.noreply.github.com>
2023-10-01 23:10:55 +0300

    REFACTOR SA_model_train.py
    Deleted code which doesn't do anything at al (section about lens-variable)
    Minor changes for more appropriate look of the program
    Shrinked in size function my_vectorizer
    Simplified some operations, made them shorter and more readable.
    Minor changes, shrinked 4 lines in 2 lines
    Added control over amount of classes in data, making it not so hardcoded, but able to be changed.
    More minor changes like comments and so on.

--- SA_model_train.py
+++ SA_model_train.py
@@ -8,6 +8,7 @@ from sklearn.metrics import accuracy_score
 from sklearn.naive_bayes import GaussianNB
 import pickle
 from words_proc import *
+import copy
 
 def load():
     encoding = "latin-1"
@@ -28,62 +29,37 @@ def load():
     return texts, y
 
 def my_vectorizer(texts, y):
-    words = {}
-    count = {}
-    total_count = {}
-    class_count = {}
+    words, count = {}, {}
+    total_count, class_count = {}, {}
     temp = 0
+    class_num = 3 #TODO: ADD COUNTING OF UNIC CLASSES INSIDE y
     for i in range(len(texts)):
         for word in texts[i]:
             if(word not in class_count):
-                class_count[word] = [0, 0, 0]
-                class_count[word][y[i]] = 1
+                class_count[word] = [0 for c in range(class_num)]
+                class_count[word][y[i]] = 1 #MARK THAT WORD APPEARS IN THIS CLASS
             else:
                 class_count[word][y[i]] += 1
-            if word not in words:
-                words[word] = temp
-                count[word] = 1
-                total_count[word] = 1
-            else:
-                count[word] +=1
-                total_count[word] += 1
-
+                
+    count = {k : sum(class_count[k]) for k in class_count.keys()}
+    total_count = copy.deepcopy(count)
     for word in class_count.keys():
-        S = sum(class_count[word])
-        for i in range(len(class_count[word])):
-            class_count[word][i] /= S
-    lens = {}
-    for word in count.keys():
-        if(count[word] in lens):
-            lens[count[word]] += 1
-        else:
-            lens[count[word]] = 1
+        class_count[word] = [class_count[word][i] / total_count[word] for i in range(len(class_count[word]))]
 
-    for word in count.keys():
-        """This values were found arbitrary by experementing""" 
-        if (count[word] < 20):
-            del(words[word])
-        elif(stop_word(word) == True):
-            del(words[word])
-        elif(total_count[word] < 20):
-            pass
-        elif(max(class_count[word]) < 0.45):
-            del(words[word])
-        elif(min(class_count[word]) > 0.25):
-            del(words[word])
- 
-    for word in words.keys():
-        words[word] = temp
-        temp = temp + 1
-
-    total_words = len(words)
-    print("Total_words = %i" % total_words)
-    result = np.zeros(shape = (len(texts), total_words), dtype = 'i4')
+    count = {word: c for word, c in count.items() if c >= 20
+             if stop_word(word) == False
+             if max(class_count[word]) >= 0.45
+             if min(class_count[word]) <= 0.25
+             } #deletion words under these criterias
+    
+    words = {word : i for i, word in enumerate(count.keys())}
+    result = np.zeros(shape = (len(texts), len(words)), dtype = 'i4')
+    threshold = 0.45
     for i in range(len(texts)):
         for word in texts[i]:
             if(word in words):
                 class_num = y[i]
-                if(class_count[word][class_num] < 0.45):
+                if(class_count[word][class_num] < threshold):
                     result[i][words[word]] = 0
                 else:
                     result[i][words[word]] = 1
@@ -92,15 +68,11 @@ def my_vectorizer(texts, y):
 texts, y = load()
 X, words = my_vectorizer(texts, y)
 classes = {}
-for i in range(len(y)):
-    if(y[i] in classes):
-        classes[y[i]] += 1
-    else:
-        classes[y[i]] = 1
 model = GaussianNB()
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state  = 76)
 model.fit(X_train, y_train)
 print("Accuracy is %f" % accuracy_score(model.predict(X_test), y_test))
+#SAVING RESULTS
 with open('model.data', 'wb') as f:
     pickle.dump(model, f)
 with open('words.data', 'wb') as f:
