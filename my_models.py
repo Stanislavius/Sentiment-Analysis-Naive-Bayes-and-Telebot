8c0e98910c0588cca4a9436310272a52d6505ca9
Stanislav <56927835+Stanislavius@users.noreply.github.com>
2023-10-04 01:47:26 +0300

    Refactor my implementation
    Make code shorter and prediction and fitting much faster
    compare to previous version

--- my_models.py
+++ my_models.py
@@ -1,34 +1,25 @@
 import numpy as np
-import collections 
+import collections
+
 class Naive_Bayes:
     def __init__(self):
         self.apriori = None
-        self.frequency = None
+        self.freq = None
 
     def fit(self, X: np.array, y: list):
-        apriori = collections.Counter(y)
-
-        self.frequency = np.zeros(shape = (len(X[0]), len(apriori)))
-        for sample, class_num in zip(X, y):
-            for word, count in enumerate(sample):
-                self.frequency[word][class_num] += count
-
-        for i in range(len(self.frequency)):
-            for j in range(len(self.frequency[i])):
-                self.frequency[i][j] /= apriori[j]
-                
-        apriori = {k:v/sum(apriori.values()) for k, v in zip(apriori.keys(), apriori.values())}
-        self.apriori = apriori
+        if self.apriori is None and self.freq is None:
+            self.apriori = np.array(list(dict(sorted(collections.Counter(y).items())).values()))
+            self.freq = np.zeros(shape = (len(self.apriori), len(X[0])))
         
-    def predict(self, X):
+        for sample, class_num in zip(X, y):
+            self.freq[class_num] += sample
+        #TODO: add recount appriori for new data or maybe not
+
+        self.freq = np.transpose(np.transpose(self.freq) / self.apriori)
+        self.apriori = self.apriori / self.apriori.sum()
+        
+    def predict(self, X: np.array):
         result = np.zeros(shape = (len(X)))
         for i in range(len(X)):
-            probs = np.zeros(shape = (len(self.apriori)))
-            for cl in self.apriori.keys():
-                ap_prob = self.apriori[cl]
-                temp_res = 0.0
-                for index, count in enumerate(X[i]):
-                    temp_res += self.frequency[index][cl]*count
-                probs[cl] = temp_res * ap_prob
-            result[i] = probs.argmax()
+            result[i] = ((self.freq * X[i]).sum(axis = 1) * self.apriori).argmax()
         return result
