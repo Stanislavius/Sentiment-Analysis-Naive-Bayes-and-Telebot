562da1834c1d6e21d9e48a8b08b6f095f9da0460
Stanislav <56927835+Stanislavius@users.noreply.github.com>
2023-10-03 16:00:18 +0300

    DEV classify documents
    DEV download documents
    Be able to download documents
    sent by users
    Be able to classify document and send back
    documents with results

--- SA_bot.py
+++ SA_bot.py
@@ -14,5 +14,18 @@ bot = telebot.TeleBot(api)
 def classify(message):
     if message.chat.type == "private":
         bot.reply_to(message, classify_text(message.text[len("classify")+1:]))
+
+@bot.message_handler(content_types=['document'])
+def document_handler(message):
+    if message.chat.type == "private":
+        bot.reply_to(message, "File is being downloaded.")
+        file_name = message.document.file_name
+        file_info = bot.get_file(message.document.file_id)
+        downloaded_file = bot.download_file(file_info.file_path)
+        file_path = "downloaded_files/" + file_name
+        with open(file_path, 'wb') as new_file:
+            new_file.write(downloaded_file)
+        result_path = classify_file(file_path)
+        bot.send_document(message.chat.id, open(result_path, 'rb'))
         
 bot.polling(non_stop=True, interval=0)
--- SA_classification.py
+++ SA_classification.py
@@ -4,19 +4,30 @@ from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from words_proc import tokenize
 
-def code_message(message_text):
+def code_messages(texts):
     total_words = len(words)
-    X = np.zeros(shape = (1, total_words), dtype = 'i4')
-    message_text = tokenize(message_text)
-    
-    for i in range(len(message_text)):
-        if message_text[i] in words:
-            X[0][list(words).index(message_text[i])] +=1
+    X = np.zeros(shape = (len(texts), total_words), dtype = 'i4')
+    texts = [tokenize(text) for text in texts]
+    for i, text in enumerate(texts):
+        for j in range(len(text)):
+            if text[j] in words:
+                X[i][list(words).index(text[j])] +=1
     return X
 
 def classify_text(message_text):
-    X = code_message(message_text)
+    X = code_messages([message_text])
     return model.predict(X)
+
+def classify_file(file_name):
+    with open(file_name, 'r') as f:
+        texts = [line for line in f.readlines()]
+    X = code_messages(texts)
+    predictions = model.predict(X)
+    results_name = "results.txt"
+    with open(results_name, 'w') as f:
+        for p in predictions:
+            f.write(str(p) + "\n")
+    return results_name
 
 with open('model.data', 'rb') as f:
     model = pickle.load(f)
