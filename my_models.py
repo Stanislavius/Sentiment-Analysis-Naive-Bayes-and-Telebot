a35fa704748481e9fdfead739aa55307b5b40784
Stanislav <56927835+Stanislavius@users.noreply.github.com>
2023-10-12 21:44:38 +0300

    DEV add OneVsRest
    Add OneVsRest class for using series of binary classifiers instead of one multiclass

--- my_models.py
+++ my_models.py
@@ -23,3 +23,30 @@ class NaiveBayes:
         for i in range(len(X)):
             result[i] = ((self.freq * X[i]).sum(axis = 1) * self.apriori).argmax()
         return result
+
+
+    def __repr__(self):
+        return "NaiveBayes()"
+
+class OneVSRest:
+    def __init__(self, model):
+        self.model = model
+
+    def fit(self, X : np.array, y: list):
+        class_num = len(set(y))
+        models = [self.model() for i in range(class_num)]
+        for i in range(class_num):
+            y_num = [0 for i in range(len(y))]
+            for j in range(len(y_num)):
+                if y[j] != i:
+                    y_num[j] = 1
+            models[i].fit(X, y_num)
+        self.models = models
+
+    def predict(self, X:np.array):
+        predictions = np.array([self.models[i].predict(X) for i in range(len(self.models))])
+        predictions = predictions.transpose()
+        return predictions.argmin(axis = 1)
+
+
+
